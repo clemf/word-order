71a45beefa2e7357c578740d7ab18026e1418d9b
Clem <clemfree@gmail.com>
2015-02-12 12:20:30 -0800

    Implement object model and sorting

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,46 @@
+var WordCount = function(word, count) {
+  this.word = word;
+  this.count = count;
+};
+
+WordCount.prototype = new WordCount();
+
 var wordOrder = function(input) {
-  var wordCount = input.split(" ");
-  return wordCount;
+
+  var wordSplit = input.split(" "),
+      uniqueWords = [];
+
+  var wordSearch = function(word) {
+    for(var i = 0; i < uniqueWords.length; i++) {
+      if(uniqueWords[i].word == word) {
+        return i;
+      }
+    }
+    return -1
+  }
+
+  wordSplit.forEach(function(word) {
+    var uniqueId = wordSearch(word);
+    if(uniqueId === -1) {
+      uniqueWords.push(new WordCount(word, 1));
+    } else {
+      uniqueWords[uniqueId].count += 1;
+    }
+  });
+
+  // console.log("uniqueWords = :", uniqueWords)
+
+  var sorted = uniqueWords.sort(function(a,b) {
+    return b.count - a.count;
+  });
+
+  // console.log("sorted = ", sorted)
+
+  var result = [];
+
+  sorted.forEach(function(object) {
+    result.push(object.word);
+  });
+
+  return result;
 };
